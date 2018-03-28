202a047b33f7aca54abe2dc9100105c8e95787ec
Michelle Olivieri <michelleo72@gmail.com>
2018-03-28 13:37:42 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,10 +16,10 @@ attr_accessor :name
   end
 
   def add_song_by_name(name)
-    song = Song.new(name)
-    self.songs << song
-    song.artist = self
-    @@song_count += 1
+    add_song(song) = Song.new(name)
+    #self.songs << song
+    #song.artist = self
+    #@@song_count += 1
   end
 
   def songs
