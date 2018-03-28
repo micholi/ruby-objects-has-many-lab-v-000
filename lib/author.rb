206555cf9610be26e38bfa5046355a4003b6acb0
Michelle Olivieri <michelleo72@gmail.com>
2018-03-28 14:05:48 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -3,30 +3,30 @@ class Author
 attr_accessor :name
 @@post_count = 0
 
-def initialize(name)
-  @name = name
-  @posts = []
-end
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
 
-def posts
-  @posts
-end
+  def posts
+    @posts
+  end
 
-def add_post(post)
-  self.posts << post
-  post.author = self
-  @@post_count += 1
-end
+  def add_post(post)
+    self.posts << post
+    post.author = self
+    @@post_count += 1
+  end
 
-def add_post_by_title(title)
-  post = Post.new(title)
-  self.posts << post
-  post.author = self
-  @@post_count += 1
-end
+  def add_post_by_title(title)
+    post = Post.new(title)
+    self.posts << post
+    post.author = self
+    @@post_count += 1
+  end
 
-def self.post_count
-  @@post_count
-end
+  def self.post_count
+    @@post_count
+  end
 
 end
