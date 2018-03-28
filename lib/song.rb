f5e67d10972799a5c43dfb914a05cce7e72799fd
Michelle Olivieri <michelleo72@gmail.com>
2018-03-28 14:05:26 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,17 +2,13 @@ class Song
 
 attr_accessor :name, :artist
 
-def initialize(name, artist = nil)
-  @name = name
-  @artist = artist
-end
+  def initialize(name, artist = nil)
+    @name = name
+    @artist = artist
+  end
 
-#def name
-#  self.name
-#end
-
-def artist_name
-  self.artist.name if artist != nil
-end
+  def artist_name
+    self.artist.name if artist != nil
+  end
 
 end
