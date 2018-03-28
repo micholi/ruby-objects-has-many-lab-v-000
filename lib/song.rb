3e193b99d56ea96bac401c2eb9e2c7a52bd93931
Michelle Olivieri <michelleo72@gmail.com>
2018-03-28 14:10:28 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,14 @@
+class Song
+
+attr_accessor :name, :artist
+
+  def initialize(name, artist = nil)
+    @name = name
+    @artist = artist
+  end
+
+  def artist_name
+    self.artist.name if artist != nil
+  end
+
+end
