9d4befae5a8ed0136f05270f6d4551bf8973b200
Michelle Olivieri <michelleo72@gmail.com>
2018-03-28 13:57:07 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -2,17 +2,17 @@ class Post
 
 attr_accessor :title, :author
 
-def initialize(title, author = nil)
-  @title = title
-  @author = author
-end
+  def initialize(title, author = nil)
+    @title = title
+    @author = author
+  end
 
-def title
-  self.title
-end
+  def title
+    self.title
+  end
 
-def author
-  self.author.title
-end
+#  def author
+#    self.author.title
+#  end
 
 end
