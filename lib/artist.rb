ad98ff9856595414c1a618200deed0174cc5e4f7
Michelle Olivieri <michelleo72@gmail.com>
2018-03-28 13:22:14 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,12 +7,13 @@ attr_accessor :name
 def initialize(name)
   @name = name
   @songs = []
+  @@song.count +=1
 end
 
 def add_song(song)
   self.songs << song
   song.artist = self
-  @@song.count +=1
+  #@@song.count +=1
 end
 
 def add_song_by_name(song)
