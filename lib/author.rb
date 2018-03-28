01fd9f02c9c9188db057da41846f8e4226126615
Michelle Olivieri <michelleo72@gmail.com>
2018-03-28 14:08:47 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -19,7 +19,8 @@ attr_accessor :name
   end
 
   def add_post_by_title(title)
-    Post.new(title) = add_post(post)
+    post = add_post(post)
+    post = Post.new(title)
     #post = Post.new(title)
     #self.posts << post
     #post.author = self
